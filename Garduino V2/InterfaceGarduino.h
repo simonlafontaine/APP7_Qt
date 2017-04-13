41f7a30f250c81c7391e5579556c69bee5eef4ff
Unknown <vincent.douville2@USherbrooke.ca>
2017-04-13 00:56:09 -0400

    ajout des signals et slots pour les boutons et l'affichage dans le status bar

--- Garduino V2/InterfaceGarduino.h
+++ Garduino V2/InterfaceGarduino.h
@@ -17,8 +17,8 @@
 #include <QSpacerItem>
 #include <QPushButton>
 #include <QStatusBar>
+#include <QString>
 #include <queue>
-
 
 class InterfaceGarduino : public QMainWindow
 {
@@ -28,11 +28,38 @@ public:
     InterfaceGarduino(QWidget *parent =0);
 
 public slots:
-    void printLog();
-//QStatusBar& statusBar, QString &message
-private:
-    //std::queue<QLabel> *file;
+    void btn1Clicked();
+    void btn1Print(const QString & );
+    void btn2Clicked();
+    void btn2Print(const QString &);
+    void btn3Clicked();
+    void btn3Print(const QString &);
+    void btn4Clicked();
+    void btn4Print(const QString &);
+    void btn5Clicked();
+    void btn5Print(const QString &);
+    void btn6Clicked();
+    void btn6Print(const QString &);
+    void btn7Clicked();
+    void btn7Print(const QString &);
+    void btn8Clicked();
+    void btn8Print(const QString &);
+    void btn9Clicked();
+    void btn9Print(const QString &);
 
+signals:
+    void btn1MsgEmit(const QString &);
+    void btn2MsgEmit(const QString &);
+    void btn3MsgEmit(const QString &);
+    void btn4MsgEmit(const QString &);
+    void btn5MsgEmit(const QString &);
+    void btn6MsgEmit(const QString &);
+    void btn7MsgEmit(const QString &);
+    void btn8MsgEmit(const QString &);
+    void btn9MsgEmit(const QString &);
+
+private:
+    const QString *btnMsg1, *btnMsg2, *btnMsg3, *btnMsg4, *btnMsg5, *btnMsg6, *btnMsg7, *btnMsg8, *btnMsg9;
 };
 
 #endif // INTERFACEGARDUINO_H
